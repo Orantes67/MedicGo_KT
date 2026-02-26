545d9516922ed3313ba2dab76425a286d0e1a4d7
Orantes67 <orantesroman39@gmail.com>
2026-02-25 19:48:16 -0600

    Cambio de importación de hilt y uso de la nueva imagen de logo en la aplicación

--- app/src/main/java/com/TiololCode/medicgo/features/login/presentation/screens/LoginScreen.kt
+++ app/src/main/java/com/TiololCode/medicgo/features/login/presentation/screens/LoginScreen.kt
@@ -35,7 +35,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.TiololCode.medicgo.R
 import com.TiololCode.medicgo.features.login.presentation.components.LicenseInputField
